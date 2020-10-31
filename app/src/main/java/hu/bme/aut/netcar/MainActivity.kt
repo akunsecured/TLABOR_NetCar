886d2cdf4b6515846af5a41ee9e8afbf31b86a2c
Pálfi Balázs <pbalazs09@gmail.com>
2020-11-01 00:10:23 +0100

    Added app starting animation

--- app/src/main/java/hu/bme/aut/netcar/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/netcar/MainActivity.kt
@@ -2,9 +2,16 @@ package hu.bme.aut.netcar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        try {
+            Thread.sleep(300)
+        }
+        catch (e: Exception){
+            e.printStackTrace()
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
