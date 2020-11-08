6623e7a93e500a1a0047085e0764413054a86a1c
Adam <61555703+akunsecured@users.noreply.github.com>
2020-11-08 22:05:41 +0100

    SettingsFragment fixed

--- app/src/main/java/hu/bme/aut/netcar/fragments/SettingsFragment.kt
+++ app/src/main/java/hu/bme/aut/netcar/fragments/SettingsFragment.kt
@@ -7,21 +7,25 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import hu.bme.aut.netcar.R
+import kotlinx.android.synthetic.main.dialog_settings.view.*
 import kotlinx.android.synthetic.main.fragment_settings.*
-import kotlinx.android.synthetic.main.nav_header_main.*
+import kotlinx.android.synthetic.main.fragment_settings.view.*
+import kotlinx.android.synthetic.main.nav_header_main.view.*
+
 
 class SettingsFragment : Fragment() {
 
     private lateinit var filepath: Uri
     private lateinit var bitmap: Bitmap
     private var chooseImage = false
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,13 +37,12 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /*
-        val header_emailText = header_email.text
-        val header_nameText = header_name.text
+        val navView: NavigationView = requireActivity().findViewById(R.id.nav_view)
+        val headerImgWidth = navView.header_image.width
+        val headerImgHeight = navView.header_image.height
 
-        settings_etEmail.hint = header_emailText
-        settings_etProfileName.hint = header_nameText
-         */
+        settings_etEmail.hint = navView.header_email.text
+        settings_etProfileName.hint = navView.header_name.text
 
         settings_userimage.setOnClickListener {
             startFileChooser()
@@ -47,37 +50,88 @@ class SettingsFragment : Fragment() {
 
         settings_btnSave.setOnClickListener {
             var detailChanges = false
-            if (settings_etEmail.text != null || settings_etPassword.text != null
-                || settings_etProfileName.text != null)
+            if (settings_etEmail.text.isNotEmpty() || settings_etPassword.text.isNotEmpty()
+                || settings_etProfileName.text.isNotEmpty()) {
                 detailChanges = true
+            }
 
             if (detailChanges) {
-                val builder = AlertDialog.Builder(view.context).setTitle("Confirm your current password")
-                val linearLayout = LinearLayout(context)
-                linearLayout.orientation = LinearLayout.VERTICAL
-                val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
-                val etPw1 = EditText(context)
-                var etPw2 = EditText(context)
-                etPw1.layoutParams = lp
-                etPw2.layoutParams = lp
-                etPw1.hint = "Password"
-                etPw2.hint = "Confirm Password"
+                val dialogLayout = LayoutInflater.from(view.context).inflate(R.layout.dialog_settings, null)
+                val builder = AlertDialog.Builder(view.context).setView(dialogLayout)
+                val alertDialog = builder.show()
 
-                linearLayout.addView(etPw1)
-                linearLayout.addView(etPw2)
+                dialogLayout.dialog_settings_btnSave.setOnClickListener {
+                    val pw: String
+                    if (dialogLayout.etPassword1.text.isEmpty()) {
+                        dialogLayout.etPassword1.requestFocus()
+                        dialogLayout.etPassword1.error = getString(R.string.btn_sigin_error_password_1)
+                    }
+                    else if (dialogLayout.etPassword2.text.isEmpty()) {
+                        dialogLayout.etPassword2.requestFocus()
+                        dialogLayout.etPassword2.error = getString(R.string.btn_sigin_error_password_1)
+                    }
+                    else if (dialogLayout.etPassword1.text.toString() != dialogLayout.etPassword2.text.toString()) {
+                        dialogLayout.etPassword2.requestFocus()
+                        dialogLayout.etPassword2.error = getString(R.string.btn_sigin_error_password_4)
+                    }
+                    else {
+                        if (chooseImage) {
+                            navView.header_image.setImageBitmap(
+                                Bitmap.createScaledBitmap(bitmap, headerImgWidth, headerImgHeight, false))
+                            settings_userimage.setImageBitmap(null)
+                        }
+                        if (settings_etEmail.text.isNotEmpty()) {
+                            navView.header_email.text = settings_etEmail.text
+                        }
+                        if (settings_etProfileName.text.isNotEmpty()) {
+                            navView.header_name.text = settings_etProfileName.text
+                        }
+                        alertDialog.dismiss()
+                        Toast.makeText(view.context, getString(R.string.changes_saved), Toast.LENGTH_LONG).show()
+                        clearSettings(view)
+                    }
+                }
 
-                builder.show()
+                dialogLayout.dialog_settings_btnCancel.setOnClickListener {
+                    clearSettings(view)
+                    if (chooseImage) {
+                        settings_userimage.setImageBitmap(null)
+                    }
+                    alertDialog.dismiss()
+                }
             }
 
-            if (chooseImage) {
-                header_image.setImageBitmap(bitmap)
+            else if (chooseImage) {
+                Toast.makeText(view.context, getString(R.string.image_saved), Toast.LENGTH_LONG).show()
+                navView.header_image.setImageBitmap(
+                    Bitmap.createScaledBitmap(bitmap, headerImgWidth, headerImgHeight, false))
+                settings_userimage.setImageBitmap(null)
             }
+
+            else {
+                Toast.makeText(view.context, getString(R.string.no_changes), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun clearSettings(view: View) {
+        if (view.settings_etEmail.text.isNotEmpty()) {
+            view.settings_etEmail.text.clear()
+        }
+        if (view.settings_etProfileName.text.isNotEmpty()) {
+            view.settings_etProfileName.text.clear()
+        }
+        if (view.settings_etPassword.text.isNotEmpty()) {
+            view.settings_etPassword.text.clear()
         }
     }
 
     private fun startFileChooser() {
         var intent = Intent().setType("image/*").setAction(Intent.ACTION_GET_CONTENT)
-        startActivityForResult(Intent.createChooser(intent, getString(R.string.choose_picture)), 111)
+        startActivityForResult(
+            Intent.createChooser(intent, getString(R.string.choose_picture)),
+            111
+        )
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
