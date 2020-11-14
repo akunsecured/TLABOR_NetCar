0707b45ec07ea0b2b5494352535e3165cba594ff
Adam <61555703+akunsecured@users.noreply.github.com>
2020-11-14 12:00:05 +0100

    Clear backend branch

--- app/src/main/java/hu/bme/aut/netcar/data/DataResult.kt
+++
@@ -1,6 +0,0 @@
-package hu.bme.aut.netcar.data
-
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-class DataResult(val id: Int?, val content: String?, val rendszam: String?, val kerek: String?, val ules: String?, val ev: String?, val marka: String?)
--- app/src/main/java/hu/bme/aut/netcar/data/Driver.kt
+++
@@ -1,5 +0,0 @@
-package hu.bme.aut.netcar.data
-
-import com.google.android.gms.maps.model.LatLng
-
-data class Driver(val name: String, val carmodel: String, val serial: String, val seats: Int, val location: LatLng)
